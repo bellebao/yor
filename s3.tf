238df9b70b56779c33d5105e470e6375e59755fd
bellebao <belle.bao@gmail.com>
2023-05-12 17:15:01 -0700

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,7 +6,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "systest-tf-log-bucket"
-  acl = "log-delivery-write"
+resource "aws_s3_bucket" "foo" {
+  #acl = "public-read-write"
+  bucket = "systest-tf-log-bucket-bb22"
+  versioning {
+    enabled = false
+  }
+  tags = merge({
+    Demo = "public-read-write"
+  }, {
+    yor_trace = "86fdf6d6-d9fb-483c-9502-e1e4f8aaa308"
+  })
 }
